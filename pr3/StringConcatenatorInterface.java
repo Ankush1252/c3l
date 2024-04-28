47edb29e989abde094635db6c8bf73cdcd54b16d
Ankush1252 <91608241+Ankush1252@users.noreply.github.com>
2024-04-28 23:54:19 +0530

    Create StringConcatenatorInterface.java

---
+++ pr3/StringConcatenatorInterface.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface StringConcatenator extends Remote {
+    String concatenateStrings(String s1, String s2) throws RemoteException;
+}
+
